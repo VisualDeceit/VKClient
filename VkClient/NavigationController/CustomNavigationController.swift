8348866b4fce71ad5ed18192f52e336e1c08a6e9
Alexander Fomin <inzhener@outlook.com>
2020-12-31 11:30:17 +0300

    Добавлен интерактивный переход

--- VkClient/NavigationController/CustomNavigationController.swift
+++ VkClient/NavigationController/CustomNavigationController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class CustomNavigationController: UINavigationController {
 
+    let interactiveTransition = InteractiveTransition()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.delegate = self
@@ -17,17 +19,28 @@ class CustomNavigationController: UINavigationController {
 
 }
 
-
 extension CustomNavigationController: UINavigationControllerDelegate {
     func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationController.Operation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
         
         switch operation {
         case .push:
+            self.interactiveTransition.animatedViewController = toVC
+            self.interactiveTransition.recognizerViewController = fromVC
             return PushNavigationControllerTransition()
         case .pop:
+            if navigationController.viewControllers.first != toVC {
+                self.interactiveTransition.recognizerViewController = fromVC
+                self.interactiveTransition.animatedViewController = toVC
+            }
             return PopNavigationControllerTransition()
         default:
             return nil
         }
+    }
+    
+    
+    func navigationController(_ navigationController: UINavigationController, interactionControllerFor animationController: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+        return interactiveTransition.hasStarted ? interactiveTransition : nil
+        
     }
 }
--- VkClient/NavigationController/CustomNavigationControllerTransitions.swift
+++ VkClient/NavigationController/CustomNavigationControllerTransitions.swift
@@ -8,6 +8,48 @@
 import UIKit
 
 
+class InteractiveTransition: UIPercentDrivenInteractiveTransition {
+    
+    var animatedViewController: UIViewController? {
+        didSet {
+            let recognizer = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(handleScreenEdgeGesture(_:)))
+            recognizer.edges = [.left]
+            animatedViewController?.view.addGestureRecognizer(recognizer)
+        }
+    }
+    
+    //по этому будет отслеживать прогресс иначе палец свернешь
+    var recognizerViewController: UIViewController?
+    
+    
+    var hasStarted: Bool = false
+    var shouldFinish: Bool = false
+
+    @objc func handleScreenEdgeGesture(_ recognizer: UIScreenEdgePanGestureRecognizer) {
+        switch recognizer.state {
+        case .began:
+            self.hasStarted = true
+            self.animatedViewController?.navigationController?.popViewController(animated: true)
+        case .changed:
+            let translation = recognizer.translation(in: recognizerViewController!.view)
+            let relativeTranslation = translation.x / (recognizerViewController!.view.bounds.width )
+            let progress = max(0, min(1, relativeTranslation))
+            
+            self.shouldFinish = progress > 0.30
+
+            self.update(progress)
+        case .ended:
+            self.hasStarted = false
+            self.shouldFinish ? self.finish() : self.cancel()
+        case .cancelled:
+            self.hasStarted = false
+            self.cancel()
+        default: return
+        }
+    }
+
+}
+
 // Вперед
 class PushNavigationControllerTransition: NSObject, UIViewControllerAnimatedTransitioning {
     
