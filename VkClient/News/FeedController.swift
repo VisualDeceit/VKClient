7f518e4325b7629525903a0d54b6dbb3329c38d9
Alexander Fomin <inzhener@outlook.com>
2021-01-04 12:32:21 +0300

    Added nameLabel & profileImageView

--- VkClient/News/FeedController.swift
+++ VkClient/News/FeedController.swift
@@ -7,27 +7,34 @@
 
 import UIKit
 
+let cellId = "cellId"
 
 class FeedController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
-    let cellId = "cellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView.backgroundColor = UIColor.red
+        collectionView.backgroundColor = .init(white: 0.9, alpha: 1)
         collectionView.register(FeedCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.alwaysBounceVertical = true
     }
     
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         myNews.count
     }
-    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        let cell =  collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! FeedCell
+        
+        cell.profileImageView.image = myNews[indexPath.item].logo
+        cell.nameLabel.text = myNews[indexPath.item].caption
+        
+        return cell
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        .init(width: view.frame.width, height: 50)
+        .init(width: view.frame.width, height: 60)
     }
 }
 
@@ -43,7 +50,42 @@ class FeedCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Simple name"
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        return label
+    }()
+    
+    let profileImageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = .red
+        return imageView
+    }()
+    
     func setupViews() {
+        backgroundColor = .white
         
+        addSubview(nameLabel)
+        addSubview(profileImageView)
+        
+        addConstrainsWithFormat(format: "H:|-8-[v0(44)]-8-[v1]|", views: profileImageView, nameLabel)
+        addConstrainsWithFormat(format: "V:|[v0]|", views: nameLabel)
+        addConstrainsWithFormat(format: "V:|-8-[v0(44)]-8-|", views: profileImageView)
+
+    }
+}
+
+
+extension UIView {
+    func addConstrainsWithFormat(format: String, views: UIView...) {
+        var viewDictionary = [String: UIView]()
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            viewDictionary[key] = view
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: [], metrics: nil, views: viewDictionary))
     }
 }
