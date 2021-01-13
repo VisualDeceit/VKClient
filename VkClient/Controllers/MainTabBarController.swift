bbc3077e796336d4fd7edae814e49091581626f1
Alexander Fomin <inzhener@outlook.com>
2021-01-13 14:17:38 +0300

    Удалены пустые строки

--- VkClient/Controllers/MainTabBarController.swift
+++ VkClient/Controllers/MainTabBarController.swift
@@ -18,19 +18,5 @@ class MainTabBarController: UITabBarController {
         feedController.navigationItem.title = "News Feed"
         self.viewControllers! += [navigationController]
 
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
