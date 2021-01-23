715a6792d7c88d732f718c9941369662f5938d24
Alexander Fomin <inzhener@outlook.com>
2021-01-23 11:37:14 +0500

    Отображение списка друзей

--- VkClient/Friends/FriendsTableViewController.swift
+++ VkClient/Friends/FriendsTableViewController.swift
@@ -13,76 +13,35 @@ protocol FriendsTableViewControllerDelegate: class {
 }
 
 class FriendsTableViewController: UITableViewController, FriendsTableViewControllerDelegate {
-    
-    var friends = [
-        User(first_name: "Александр", last_name: "Фомин"),
-        User(first_name: "Юрий", last_name: "Султанов"),
-        User(first_name: "Никита", last_name: "Сергеев"),
-        User(first_name: "Артем", last_name: "Зарудный"),
-        User(first_name: "Виталий", last_name: "Степушин"),
-        User(first_name: "Юрий", last_name: "Фёдоров"),
-        User(first_name: "Татьяна", last_name: "Сундукова"),
-        User(first_name: "Даниил", last_name: "Книсс"),
-        User(first_name: "Хасан", last_name: "Сатийаджиев"),
-        User(first_name: "Никита", last_name: "Максимов"),
-        User(first_name: "Игорь", last_name: "Гомзяков"),
-        User(first_name: "Василий", last_name: "Князев"),
-        User(first_name: "Артём", last_name: "Сарана"),
-        User(first_name: "Денис", last_name: "Юрчик"),
-        User(first_name: "Роман", last_name: "Вертячих"),
-        User(first_name: "Айрат", last_name: "Бариев"),
-        User(first_name: "Никита", last_name: "Грас"),
-        User(first_name: "Дмитрий", last_name: "Коврига"),
-        User(first_name: "Никита", last_name: "Кулигин"),
-        User(first_name: "Никита", last_name: "Рухайло"),
-        User(first_name: "Никита", last_name: "Кочнев"),
-        User(first_name: "Кирилл", last_name: "Васильев"),
-        User(first_name: "Сергей", last_name: "Лысов"),
-        User(first_name: "Руслана", last_name: "Иванова"),
-        User(first_name: "Наталья", last_name: "Филимонова"),
-        User(first_name: "Максим", last_name: "Тулинов"),
-        User(first_name: "Юрий", last_name: "Егоров"),
-        User(first_name: "Евгений", last_name: "Дербенев"),
-        User(first_name: "Дмитрий", last_name: "Борисов"),
-        User(first_name: "Игорь", last_name: "Могонов"),
-        User(first_name: "Николай", last_name: "Устинов"),
-        User(first_name: "Сергей", last_name: "Нелюбин"),
-        User(first_name: "Виктор", last_name: "Гарицкий"),
-        User(first_name: "Николай", last_name: "Устинов"),
-        User(first_name: "Лев", last_name: "Бажков"),
-        User(first_name: "Алексей", last_name: "Нестеров"),
-        User(first_name: "Роман", last_name: "Устинов"),
-        User(first_name: "Николай", last_name: "Перепел"),
-    ]
-    
-    var friends0 = [User0]()
+  
+    var friends = [User0]()
     
     @IBOutlet weak var searchBar: UISearchBar!
     
     var friendsLastNameTitles = [String]() //массив начальных букв sections
-    var friendsDictionary = [String: [User]]()  //словарь
-    var filtredFriendsDictionary = [String: [User]]() //for display
+    var friendsDictionary = [String: [User0]]()  //словарь
+    var filtredFriendsDictionary = [String: [User0]]() //for display
     
     //реализуем протокол FriendsTableViewControllerDelegate
     func update(indexPhoto: Int, like: Like) {
         //получаем данные из делегата
         let lastNameKey = friendsLastNameTitles[tableView.indexPathForSelectedRow!.section]
-        if var userValues = friendsDictionary[lastNameKey] {
-            userValues[tableView.indexPathForSelectedRow!.row].album![indexPhoto].like = like
-            friendsDictionary[lastNameKey] = userValues
-            filtredFriendsDictionary[lastNameKey] = userValues
-        }
+//        if var userValues = friendsDictionary[lastNameKey] {
+//            userValues[tableView.indexPathForSelectedRow!.row].album![indexPhoto].like = like
+//            friendsDictionary[lastNameKey] = userValues
+//            filtredFriendsDictionary[lastNameKey] = userValues
+//        }
     }
     
     //
-    private func splitOnSections(for inputArray: [User]) -> ([String], [String: [User]]) {
+    private func splitOnSections(for inputArray: [User0]) -> ([String], [String: [User0]]) {
         
         var sectionsTitle = [String]()
-        var sectionData = [String: [User]]()
+        var sectionData = [String: [User0]]()
        
         //разбираем исходный массив в словарь для индексации таблицы
         for user in inputArray {
-            let lastNameKey = String(user.last_name.prefix(1))
+            let lastNameKey = String(user.lastName.prefix(1))
             if var userValues = sectionData[lastNameKey] {
                 userValues.append(user)
                 sectionData[lastNameKey] = userValues
@@ -101,10 +60,6 @@ class FriendsTableViewController: UITableViewController, FriendsTableViewControl
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // разбор исходных данных
-        (friendsLastNameTitles, friendsDictionary) = splitOnSections(for: friends)
-        //copy dictionary for display
-        filtredFriendsDictionary = friendsDictionary
         //регистрируем кастомный хедер
         tableView.register(MyCustomSectionHeaderView.self, forHeaderFooterViewReuseIdentifier: "sectionHeader")
      
@@ -112,8 +67,15 @@ class FriendsTableViewController: UITableViewController, FriendsTableViewControl
         
         let networkService = NetworkServices()
         
-        networkService.getUserFriends { [weak self] users in
-            self?.friends0 = users
+        networkService.getUserFriends {friends in
+            self.friends = friends
+            DispatchQueue.main.async {
+                // разбор исходных данных
+                (self.friendsLastNameTitles, self.friendsDictionary) = self.splitOnSections(for: friends)
+                //copy dictionary for display
+                self.filtredFriendsDictionary = self.friendsDictionary
+                self.tableView?.reloadData()
+            }
         }
         
     }
@@ -142,7 +104,7 @@ class FriendsTableViewController: UITableViewController, FriendsTableViewControl
         //передаем данные в ячейку
         let lastNameKey = friendsLastNameTitles[indexPath.section]
         if let userValues = filtredFriendsDictionary[lastNameKey] {
-            cell.friendName.text = "\(userValues[indexPath.row].first_name) \(userValues[indexPath.row].last_name)"
+            cell.friendName.text = "\(userValues[indexPath.row].firstName) \(userValues[indexPath.row].lastName)"
             cell.friendAvatar.logoView.image = #imageLiteral(resourceName: "camera_50")
         }
         
@@ -164,12 +126,12 @@ class FriendsTableViewController: UITableViewController, FriendsTableViewControl
               let controller = segue.destination as? PhotoCollectionViewController else { return }
         
         //передача данных в PhotoCollectionViewController
-        let selectedUser = tableView.indexPathForSelectedRow
-        let lastNameKey = friendsLastNameTitles[selectedUser!.section]
-        if let userValues = filtredFriendsDictionary[lastNameKey] {
-            controller.user = userValues[selectedUser!.row]
-        }
-        controller.delegate = self // подписали на делегат
+//        let selectedUser = tableView.indexPathForSelectedRow
+//        let lastNameKey = friendsLastNameTitles[selectedUser!.section]
+//        if let userValues = filtredFriendsDictionary[lastNameKey] {
+//            controller.user = userValues[selectedUser!.row]
+//        }
+//        controller.delegate = self // подписали на делегат
 
     }
     
@@ -196,8 +158,8 @@ extension FriendsTableViewController: UISearchBarDelegate {
         
         filtredFriendsDictionary = friendsDictionary.mapValues{
             $0.filter {
-                $0.first_name.lowercased().contains(searchText.lowercased()) ||
-                    $0.last_name.lowercased().contains(searchText.lowercased())
+                $0.firstName.lowercased().contains(searchText.lowercased()) ||
+                    $0.lastName.lowercased().contains(searchText.lowercased())
             }
         }.filter {!$0.value.isEmpty}
         
