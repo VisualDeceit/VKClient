84deb9d45048d744feac06594ed7cc568e62f145
Alexander Fomin <inzhener@outlook.com>
2021-01-27 22:12:14 +0500

    UserPhoto преобразован в объект Realm

--- VkClient/Models/UserPhoto.swift
+++ VkClient/Models/UserPhoto.swift
@@ -7,6 +7,7 @@
 
 
 import Foundation
+import RealmSwift
 
 struct UserPhotoModel: Decodable {
     let response: UserPhotoResponse
@@ -16,11 +17,11 @@ struct UserPhotoResponse: Decodable {
     let items: [UserPhoto]
 }
 
-struct UserPhoto: Decodable {
-    let likesCount: Int
-    let isLiked: Int
-    let repostsCount: Int
-    var sizes: [PhotoSize]
+class UserPhoto: Object, Decodable {
+    @objc dynamic var likesCount: Int = 0
+    @objc dynamic var isLiked: Int = 0
+    @objc dynamic var repostsCount: Int = 0
+    var sizes = List<PhotoSize>()
     
     enum RepostsCodingKeys: String, CodingKey{
         case repostsCount = "count"
@@ -40,9 +41,11 @@ struct UserPhoto: Decodable {
         case reposts
     }
       
-    init(from decoder: Decoder) throws {
+    convenience required init(from decoder: Decoder) throws {
+        self.init()
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.sizes = try container.decode([PhotoSize].self, forKey: .sizes)
+        let sizesArray = try container.decode([PhotoSize].self, forKey: .sizes)
+        sizesArray.forEach{ self.sizes.append($0) }
         
         let likeContainer = try container.nestedContainer(keyedBy: LikesCodingKeys.self, forKey: .likes)
         self.isLiked = try likeContainer.decode(Int.self, forKey: .isLiked)
@@ -53,20 +56,10 @@ struct UserPhoto: Decodable {
     }
 }
 
-//struct Likes: Decodable {
-//    let count: Int
-//    let isLiked: Int
-//
-//    enum CodingKeys: String, CodingKey {
-//        case count
-//        case isLiked = "user_likes"
-//    }
-//}
-
-struct PhotoSize: Decodable {
-    let height: Int
-    let width: Int
-    let type: String
-    let url: String
+class PhotoSize: Object, Decodable {
+    @objc dynamic var height: Int = 0
+    @objc dynamic var width: Int = 0
+    @objc dynamic var type: String = ""
+    @objc dynamic var url: String = ""
 }
 
