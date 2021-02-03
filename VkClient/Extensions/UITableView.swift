97ee5a2d1b8450bc6628d01ffa8eb2a097919f9d
Alexander Fomin <inzhener@outlook.com>
2021-02-03 16:48:55 +0500

    Расширение для UITableView

---
+++ VkClient/Extensions/UITableView.swift
@@ -0,0 +1,27 @@
+//
+//  UITableView.swift
+//  VkClient
+//
+//  Created by Alexander Fomin on 03.02.2021.
+//
+
+import UIKit
+import RealmSwift
+
+extension UITableView {
+  func applyChanges<T>(changes: RealmCollectionChange<T>) {
+    switch changes {
+      case .initial: reloadData()
+      case .update(_, let deletions, let insertions, let updates):
+        let fromRow = { (row: Int) in return IndexPath(row: row, section: 0) }
+        
+        beginUpdates()
+        insertRows(at: insertions.map(fromRow), with: .automatic)
+        reloadRows(at: updates.map(fromRow), with: .automatic)
+        deleteRows(at: deletions.map(fromRow), with: .automatic)
+        endUpdates()
+      case .error(let error): fatalError("\(error)")
+    }
+  }
+}
+
